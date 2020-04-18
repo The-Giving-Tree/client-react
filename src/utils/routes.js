42184009cb86f648322dd7472df6c624599c3836
gmaijoe <gmai95h@gmail.com>
2020-04-17 20:57:35 -0700

    adding sandbox/staging

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -7,9 +7,9 @@ const ROUTES = {
     giving_tree: 'http://localhost:3000',
     socket: 'http://localhost:3000'
   },
-  sandbox: {
-    giving_tree: 'http://localhost:3000',
-    socket: 'http://localhost:3000'
+  sandbox: { // staging as well
+    giving_tree: 'https://api-staging.givingtreeproject.org',
+    socket: 'https://api-staging.givingtreeproject.org'
   },
   local: {
     giving_tree: 'http://localhost:3000',
